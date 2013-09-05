8348e30820a8583333ca7167005f708a886943db
TraficTune <TraficTune@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-09-05 00:21:30 +0000

    'trunk/readme.txt'
    git-svn-id: https://plugins.svn.wordpress.org/facebook-iframe-pro/trunk@767155 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- FacebookIframePro.php
+++ FacebookIframePro.php
@@ -1,1 +1,22 @@
-﻿<?php /* Plugin Name: Facebook iFrame Pro Plugin URI: http://www.trafictune.comDescription: Incroyable Plugin Wordpress qui vous permet de générer des Fanpages IFRAME Facebook en 5 Minutes. Ajouter votre Code d'auto-répondeur automatique. Inclut PDF & Instructions vidéo inclus.Version: 1.2 Author: AbdellahAuthor URI: http://www.trafictune.com*/ /* Options rebrandable */ $pluginslug = 'FacebookIframePro'; // Cela devrait être le nom du dossier principal de le plugin. Par exemple `wp-content/plugins/{$pluginslug}/`. $pluginname = 'Facebook iFrame Pro'; // This is the name of the plugin that is displayed publicly. This should be the same as above in the plugin's headers. $pluginurl = 'http://www.fbiframepro.com'; // This is the URL to the plugin's main website. This variable is needed for displaying it on the site since I can't get the header information, and is the same as the plugin's headers above. $helplink = 'Click Here To Download Your Unadvertised Bonuses'; // This is the text of the 'help' link. $helpurl = 'http://www.fbiframepro.com/bonus/'; // This is the URL to the 'help' website. $affbanner = 'http://www.fbiframepro.com/images/global-success-club.gif'; // This is a URL to an image, displayed on the WP Dashboard under the `Fan Pages > Instructions` page. $affurl = 'http://scrnch.me/gscplus'; // The banner specified above will link to this URL on the Instructions page. $showbanner = True; // If false, the banner will not appear on the Instructions page. $showfooter = True; // If false, the footer will not appear on any pages. /* End Rebrandable Options */ include 'efpd/functions.php'; // Include everything! ?>
+﻿<?php 
+/* 
+Plugin Name: Facebook iFrame Pro 
+Plugin URI: http://www.media-infoproduit.com/
+Description: Facebook iFrame Pro™ est un Plugin Wordpress facile à utiliser qui vous permet de générer des Fanpages IFRAME en 5 Minutes. Vous pouvez également jouter un Code d'auto-répondeur automatique. Inclut PDF & Instructions vidéo. 
+Version: 1.2 
+Author: Abdellah 
+Author URI: http://www.media-infoproduit.com/
+*/ 
+/* Rebrandable Options */ 
+$pluginslug = 'FacebookIframePro'; // This should be the plugin's main folder name. For example `wp-content/plugins/{$pluginslug}/`. 
+$pluginname = 'Facebook iFrame Pro'; // This is the name of the plugin that is displayed publicly. This should be the same as above in the plugin's headers. 
+$pluginurl = 'http://www.media-infoproduit.com/'; // This is the URL to the plugin's main website. This variable is needed for displaying it on the site since I can't get the header information, and is the same as the plugin's headers above. 
+$helplink = 'Click Here To Download Your Unadvertised Bonuses'; // This is the text of the 'help' link. 
+$helpurl = 'http://www.media-infoproduit.com/bonus/'; // This is the URL to the 'help' website. 
+$affbanner = 'http://www.fbiframepro.com/images/global-success-club.gif'; // This is a URL to an image, displayed on the WP Dashboard under the `Fan Pages > Instructions` page. 
+$affurl = 'http://www.media-infoproduit.com/'; // The banner specified above will link to this URL on the Instructions page. 
+$showbanner = True; // If false, the banner will not appear on the Instructions page. 
+$showfooter = True; // If false, the footer will not appear on any pages. 
+/* End Rebrandable Options */ 
+include 'efpd/functions.php'; // Include everything! 
+?>
--- readme.txt
+++ readme.txt
@@ -1,12 +1,12 @@
 /*
 Plugin Name: Facebook iFrame Pro™ WP-Plugin
-Plugin Script: poptunetrafic.php
-Plugin URI: http://www.trafictune.com
-Description: Découvrez comment vous pouvez faire apparaitre votre site web dans les comptes sociaux de plus de 1 milliard d'utilisateurs sur Twitter, Facebook, Google Plus et maintenant LinkedIn ! Rendre votre trafic travaillent pour vous ! Générer des tonnes de trafic des sites de médias sociaux et backlinks rapidement et facilement.
+Plugin Name: Facebook iFrame Pro 
+Plugin URI: http://www.fbiframepro.com 
+Description: Facebook iFrame Pro™ est un Plugin Wordpress facile à utiliser qui vous permet de générer des Fanpages IFRAME en 5 Minutes. Vous pouvez également jouter un Code d'auto-répondeur automatique. Inclut PDF & Instructions vidéo.
+Author: Abdellah
+Author URI: http://www.trafictune.com 
 
 Version: 0.1.2 
-Author: Abdellah
-Author URI: http://www.trafictune.com
 
 --- CE PLUGIN ET TOUS LES FICHIERS INCLUS SONT COPYRIGHT © 2011. 
 VOUS NE POUVEZ PAS MODIFIER, REVENDRE, DISTRIBUER OU COPIER CE CODE EN QUELQUE SORTE. ---
